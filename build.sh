90af15d2f831dd9161063e8a25b68583c52ac47b
Nicholas Buoncristiani <nick.buoncristiani@eluv.io>
2025-12-18 21:11:31 +0000

    updated build.sh

--- build.sh
+++ build.sh
@@ -2,15 +2,6 @@
 
 echo keys:
 
-if ! ssh-add -l ; then
-    echo ssh agent does not have any identities loaded, will not be able to build
-    echo add them by running ssh-add on your local machine, or on the remote if you have keys there
-    echo you may also need to restart vs code and the remote server for this to work
-    exit 1
-fi
-
-echo
-
 set -e
 
 SCRIPT_PATH="$(dirname "$(realpath "$0")")"
@@ -23,4 +14,4 @@ MODEL_PATH=$(yq -r .storage.model_path $SCRIPT_PATH/config.yml)
 mkdir -p models
 rsync --progress --update --times --recursive --links --delete $MODEL_PATH/ $SCRIPT_PATH/models/stt/
 
-time podman build --format docker -t asr . --network host --build-arg SSH_AUTH_SOCK=/tmp/ssh-auth-sock --volume "${SSH_AUTH_SOCK}:/tmp/ssh-auth-sock"
+buildscripts/build_container.bash -t "asr:${IMAGE_TAG:-latest}" -f Containerfile .
